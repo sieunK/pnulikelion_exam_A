49b82f78874c85c4831be7c5c06b0a3497482bd3
chichi <jchh2036@naver.com>
2019-11-15 21:59:09 +0900

    pnulikelion A group test

--- exam/models.py
+++ exam/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Seed(models.Model):
-    fake_data = models.TextField()
+    fake_data = models.TextFiled()
--- testproject/urls.py
+++ testproject/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import exam.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', exam.views.index, name="index"),
