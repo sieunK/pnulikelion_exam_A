cd1e3d7744ebc699448072beafb83ef17612f9d6
sieunK <april908@naver.com>
2019-11-18 21:27:40 +0900

    pushpush baby

---
+++ exam/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Seed(models.Model):
+    fake_data = models.TextField()
+    
---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\april\desktop\likelion_coding_test\pnulikelion_exam_a-master\pnulikelion_exam_a-master\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
